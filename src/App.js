28e415754c4c7d38f9d07524d21c2c5b4d0ae3e1
Hatice-Altun <van.altun@outlook.com>
2023-12-17 12:59:35 +0100

    Slider Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageSlider from "./components/ImageSlider";
+
+// npm install -D tailwindcss
+// npm install i react-icons --save
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageSlider/>
     </div>
   );
 }
